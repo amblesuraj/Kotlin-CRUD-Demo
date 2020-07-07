28d1510f566dceca242c5b5e820c13fcdb534b97
Suraj <amblesuraj@gmail.com>
2020-07-07 19:23:19 +0530

    Kotlin Project updated : CRUD completed

--- src/main/kotlin/com/ngu/Controller/StudentController.kt
+++ src/main/kotlin/com/ngu/Controller/StudentController.kt
@@ -9,38 +9,62 @@ import org.springframework.validation.BindingResult
 import org.springframework.validation.Errors
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.mvc.support.RedirectAttributes
+import java.lang.IllegalArgumentException
+import java.util.*
 import javax.validation.Valid
 
 @Controller
 class StudentController {
     @Autowired
-    lateinit var stduentService: StudentService
+    lateinit var studentService: StudentService
 
 
     @GetMapping(value = ["","/","index"])
     fun index(model: Model) :String {
         model.addAttribute("student",Student())
-        model.addAttribute("students",stduentService.findAllStudents())
+        model.addAttribute("students",studentService.findAllStudents())
         return "index"
     }
+
+    @GetMapping(value = ["/addStudent"])
+    fun addStudent(model: Model) :String {
+        model.addAttribute("student",Student())
+        model.addAttribute("students",studentService.findAllStudents())
+        return "addStudent"
+    }
+
     @PostMapping("/save")
     fun addStudent(@ModelAttribute("student") @Valid student: Student, model: Model,
                    errors : Errors, redirectAttributes: RedirectAttributes) : String{
         if(errors.hasErrors())
         {
-            model.addAttribute("msg","Something went wrong")
-            return "index"
+            model.addAttribute("error","Something went wrong")
+            return "addStudent"
         }
+        if(student.id == null){
+            studentService.addStudent(student)
+            redirectAttributes.addFlashAttribute("msg","Student added Successfully")
+            return "redirect:/addStudent?added=${student.id}"
+        }
+        studentService.addStudent(student)
+        redirectAttributes.addFlashAttribute("msg","${student.name} your data Updated Successfully")
+        return "redirect:/addStudent?updated=${student.id}"
 
-        stduentService.addStudent(student)
-        redirectAttributes.addFlashAttribute("msg","Student added Successfully")
-        return "redirect:/?added=${student.id}"
     }
 
     @DeleteMapping("/delete/{id}")
     fun deleteStudent(@PathVariable id : Int , model: Model) : String
     {
-        stduentService.deleteById(id)
+
+        studentService.deleteById(id)
+
         return "redirect:/?deleted=$id"
     }
+
+    @GetMapping("/update/{id}")
+    fun showStudentDetails(@PathVariable id: Int,model: Model) : String {
+        model.addAttribute("student",studentService.findById(id))
+        return "addStudent"
+    }
+
 }
